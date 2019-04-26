3ef90ed6244d57d45f4938c50230ccc557ac869c
unknown <amanda.wisniowski@gmail.com>
2019-04-25 21:10:50 -0500

    adding in react native app auth

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -7,14 +7,101 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import {UIManager, StyleSheet, View } from 'react-native';
 import AppContainer from './src/screens/AppContainer';
+import { authorize, refresh, revoke } from 'react-native-app-auth';
+
+const credentials = require('./credentials');
+
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+
+type State = {
+  hasLoggedInOnce: boolean,
+  accessToken: ?string,
+  accessTokenExpirationDate: ?string,
+  refreshToken: ?string
+};
+
+const config = {
+  issuer: 'https://accounts.spotify.com/authorize?',
+  clientId: credentials.clientId,
+  redirectUrl: credentials.redirectUrl,
+  clientSecret: credentials.clientSecret,
+  scopes: ['user-library-read','app-remote-control']
+};
 
 
 
-export default class App extends Component{
+export default class App extends Component<{}, State> {
+  state = {
+    hasLoggedInOnce: false,
+    accessToken: '',
+    accessTokenExpirationDate: '',
+    refreshToken: ''
+  };
 
   
+  animateState(nextState: $Shape<State>, delay: number = 0) {
+    setTimeout(() => {
+      this.setState(() => {
+        LayoutAnimation.easeInEaseOut();
+        return nextState;
+      });
+    }, delay);
+  }
+
+  authorize = async () => {
+    try {
+      const authState = await authorize(config);
+
+      this.animateState(
+        {
+          hasLoggedInOnce: true,
+          accessToken: authState.accessToken,
+          accessTokenExpirationDate: authState.accessTokenExpirationDate,
+          refreshToken: authState.refreshToken,
+          scopes: authState.scopes
+        },
+        500
+      );
+    } catch (error) {
+      Alert.alert('Failed to log in', error.message);
+    }
+  };
+
+  refresh = async () => {
+    try {
+      const authState = await refresh(config, {
+        refreshToken: this.state.refreshToken
+      });
+
+      this.animateState({
+        accessToken: authState.accessToken || this.state.accessToken,
+        accessTokenExpirationDate:
+          authState.accessTokenExpirationDate || this.state.accessTokenExpirationDate,
+        refreshToken: authState.refreshToken || this.state.refreshToken
+      });
+    } catch (error) {
+      Alert.alert('Failed to refresh token', error.message);
+    }
+  };
+
+  revoke = async () => {
+    try {
+      await revoke(config, {
+        tokenToRevoke: this.state.accessToken,
+        sendClientId: true
+      });
+      this.animateState({
+        accessToken: '',
+        accessTokenExpirationDate: '',
+        refreshToken: ''
+      });
+    } catch (error) {
+      Alert.alert('Failed to revoke token', error.message);
+    }
+  };
 
   render() {
     return (
